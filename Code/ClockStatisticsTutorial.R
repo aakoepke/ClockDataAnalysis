4ca5b65bdacf412ee325b07be646b8f1939b267f
Berry <cmb15@NIST.GOV>
2022-07-06 10:30:46 -0600

    spectral analysis for clock data

--- Code/ClockStatisticsTutorial.R
+++ Code/ClockStatisticsTutorial.R
@@ -26,7 +26,7 @@ plot(1:N, x.t, type = "l")
 #periodogram spectral estimate
 S.x.hat <- spec.pgram(x.t, plot = FALSE)
 
-plot(log10(S.x.hat$freq),log10(S.x.hat$spec))
+plot(log10(S.x.hat$freq),log10(S.x.hat$spec), type = "l")
 
 #linear regresssion
 
@@ -37,6 +37,26 @@ summary(ln.reg)
 abline(a = ln.reg$coefficients[1], b = ln.reg$coefficients[2])
 
 plot(ln.reg)
+
+
+#Random Walk (ARFIMA(0,1,0))
+f <- seq(-0.5,0.5, by = 0.01)
+plot(f,abs(1-exp(-j*2*pi*f))^2, type = "l")
+lines(f, abs(2*sin(pi*f))^2, col = "blue")
+
+spec.RW <- function(f,sigma.e = 1){
+  return(sigma.e/abs(2*sin(pi*f))^2)
+}
+
+lines(log10(f),log10(spec.RW(f)), col = "blue")
+par(mfrow = c(2,1))
+
+
+#White Noise
+spec.WN <- function(sigma.e = 1,N){
+  return(rep(sigma.e,times = N))
+}
+lines(log10(f), log10(spec.WN(1,N = length(f))), col = "blue")
 
 
 
