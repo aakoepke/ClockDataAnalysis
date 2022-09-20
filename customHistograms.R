85a8dc202eb913ffe261b7a9fcf11d21703336fd
Koepke <aak3@nist.gov>
2022-09-20 08:22:54 -0600

    playing with histograms

--- customHistograms.R
+++ customHistograms.R
@@ -19,15 +19,30 @@ sumDat=dat %>% group_by(method,tau) %>%
             max=max(measurement))
 
            
+histwidth=.2
 ggplot(data = sumDat,aes(x=tau,y=median,col=method,fill=method))+
   ### width controls how wide these are, can change all below if they are too wide to see tau linear relationship well with real data
-  geom_errorbar(aes(ymin=min,ymax=max),width=.05,position = "dodge")+
-  geom_crossbar(aes(ymin=lower25,ymax=upper75),width=.05,position = "dodge")+
-  geom_crossbar(aes(ymin=lower25,ymax=upper75),color="black",width=.05,position = "dodge")+
+  geom_errorbar(aes(ymin=min,ymax=max),width=histwidth,position = "dodge")+
+  geom_crossbar(aes(ymin=lower25,ymax=upper75),width=histwidth,position = "dodge")+
+  geom_crossbar(aes(ymin=lower25,ymax=upper75),color="black",width=histwidth,position = "dodge")+
   ####
   scale_y_log10()+
   scale_x_log10()+
-  ylab(expression(log[10]("some nonsense")))+
-  xlab(expression(log[10](tau)))+
+  ylab("adev")+
+  xlab(expression(tau))+
   ### add true line below
-  geom_abline(slope = .01,intercept = 1,size=1)
+  geom_abline(slope = .01,intercept = 1,size=1)+
+  guides(fill=guide_legend(title="title"),color=guide_legend(title="title"))
+  # scale_color_manual(values = c("Method A name","Method B name","Method C name"))#,col="legend title")
+
+
+
+
+
+################## new hists, transform first
+
+dat$log10tau=(log10(dat$tau))
+dat$log10meas=log10(dat$measurement)
+
+ggplot(dat,aes(log10tau,log10meas,col=method,group=log10tau))+
+  geom_boxplot()
