87758980f49696f30e9487a97fe441dae3a74714
caitberry <cberry1882@gmail.com>
2023-02-01 13:38:40 -0700

    Code for AVAR estimate and calculating variance of said estimate

--- Code/AVARSpectralEstimate.R
+++ Code/AVARSpectralEstimate.R
@@ -1,14 +1,113 @@
-##### Calculate sigma.hat_tau using the Spectral Estimate ########
+#######################################################################################
+##### Calculate sigma.hat_tau (the Allan Variance) using the Spectral Estimate ########
+#######################################################################################
 
+###################
+#### libraries ####
+###################
+
+library(tidyverse)
+library(RSpectra)
+library(fields)
+
+###########################
+#### things we'll need ####
+###########################
+
+## i
 im <- complex(real = 0, imaginary = 1)
-#Calculate sigma.hat_tau using transfer function
 
+## MTSE for gappy data function 
+multitaper_est <- function(X.t, W, K){
+  X.t <- X.t - mean(X.t, na.rm = TRUE) #demean
+  N.long <- length(X.t)
+  t.n <- 1:N.long
+  missing.indices <- which(is.na(X.t))
+  t.n[which(is.na(X.t))] <- NA
+  
+  
+  dist.mat <- rdist(na.omit(t.n))
+  
+  #create the A' matrix (Chave 2019 equation (22))
+  A.prime <- (1/(pi*dist.mat))*sin(2*pi*W*dist.mat)
+  A.prime[row(A.prime) == col(A.prime)] <- W*2
+  print("A matrix computed")
+  
+  eigdec <- eigs(A.prime, k = K, which = "LM")
+  eig_vecs <- eigdec$vectors #get only the vectors
+  print("tapers computed")
+  
+  if(K ==1){
+    if (mean(Re(eig_vecs))<0){
+      eig_vecs <- -eig_vecs
+    }
+  }
+  
+  if(K == 2  || K == 3){
+    
+    if (mean(Re(eig_vecs[,1]))<0){
+      eig_vecs[,1] <- -eig_vecs[,1]
+    }
+    if (Re(eig_vecs[2,2] - eig_vecs[1,2])<0){
+      eig_vecs[,2] <- -eig_vecs[,2]
+    }
+    
+    if(K == 3){
+      if (mean(Re(eig_vecs[,3]))<0){
+        eig_vecs[,3] <- -eig_vecs[,3]
+      }
+    }
+  }
+  if(K >=4){
+    #some sign maintenance
+    for(i in seq(1,K,by = 2)){
+      if (mean(Re(eig_vecs[,i]))<0){
+        eig_vecs[,i] <- -eig_vecs[,i]
+      }
+    }
+    
+    for(i in seq(2,K-1,by = 2)){
+      if (Re(eig_vecs[2,i] - eig_vecs[1,i])<0){
+        eig_vecs[,i] <- -eig_vecs[,i]
+      }
+    }
+  }
+  
+  print("sign maintenance done")
+  
+  ##use tapers to generate spectral estimate
+  N <- length(na.omit(t.n))
+  S.x.hat_MD <- rep(NA, times = floor(N/2) + 1)
+  
+  for(j in 0:floor(N/2)){
+    k.vec <- rep(NA,times = K)
+    for(k in 0:(K-1)){
+      W.t <- eig_vecs[,k+1]*na.exclude(X.t)
+      inner.sum <- sum(W.t*exp(-complex(real = 0, imaginary = 1)*2*pi*na.omit(t.n)*j/N), na.rm = TRUE)
+      k.vec[k + 1] <- abs(inner.sum)^2
+    }
+    S.x.hat_MD[j+1] <- mean(k.vec)
+  }
+  
+  
+  return(list("tapers" = eig_vecs, "e.values" = eigdec$values, "spectrum" = S.x.hat_MD))
+}
+
+
+######################################################################################################
+###### Calculate sigma.hat_tau using transfer function method (see "reappraisal...", page 70) ########
+######################################################################################################
+
+#G_tau(f) function
 transfer.func <- function(f,tau){
   4*sin(pi*f*tau)^4/(tau*sin(pi*f))^2
 }
 
-#input: spectral estimate, taus where you want the AVAR calculated
+###########   Allan Variance Calculation    #############
+######## (eq'n at bottom of p. 70 of reappraisal) #######
+#input: spectral estimate (as a vector), taus (as a vector) where you want the AVAR calculated
 #output: a vector of the AVAR estimates
+
 AVAR_trfunc <- function(spectral_est, taus){
 out <- rep(NA, times = length(taus))
 f <- seq(0,0.5,length.out = length(spectral_est))
@@ -21,6 +120,81 @@ out[i] <- f[2]*sum(G.vec*spectral_est)
 
 return(out)
 }
+
+
+
+######## Calculating the variance of this estimate of the AVAR ########
+
+
+### to test out function below, run the following lines:
+
+#make a white noise process and add some NA values
+X.t_missing <- rnorm(100)
+X.t_missing[c(8:20,51:70)] <- NA
+
+#get spectral estimate
+spec.est <- multitaper_est(X.t_missing,W=0.09, K = 5)
+tapers <- spec.est$tapers
+t.vec <- 1:100
+t.n <- t.vec[which(!is.na(X.t_missing))]
+X.t <-  na.omit(X.t_missing)
+taus <-  2^(0:6)
+
+
+#this is where the function that calculates the variance of the
+#allan variance estimate, which is in progress:
+
+#var_AVAR_trfunc <- function(tapers, t.n, X.t, taus){
+
+saved <- acf(X.t_missing, na.action = na.pass, lag.max = 100)
+#distance matrix of time points
+dist.mat <- rdist(t.n)
+#R.x matrix
+X.t <- X.t - mean(X.t)
+L <- length(X.t)
+R.x <- matrix(saved$acf[dist.mat+1], nrow = L, ncol = L)
+#f vector
+f <- seq(0,0.5, length.out = floor(length(t.n))/2 + 1)
+
+
+#Calculate Cov matrix C
+N <- length(f)
+Cov.mat <- matrix(NA, nrow = N, ncol = N)
+W_matlist <- list()
+
+for(i in 1:N){
+  print(i)
+  W_matlist[[i]] <- t(tapers)%*%exp(-im*2*pi*f[i]*dist.mat)
+}
+
+for(i in 1:N){
+  print(i)
+  j = i
+  while(j>=i & j <=N){
+    W.star <- t(Conj(W_matlist[[i]]))
+    W <- W_matlist[[j]]
+    #Cov.mat[i,j] <- sum(abs(W.star%*%R.x%*%W)^2) #frobenius norm 
+    Cov.mat[i,j] <- sqrt(sum(abs(W%*%diag(x = 1, nrow = 67, ncol = 67)%*%W.star)^2)) #2-norm
+    j = j + 1
+  }
+}
+
+Cov.mat[lower.tri(Cov.mat)] <- t(Cov.mat)[lower.tri(Cov.mat)]
+
+out <- rep(NA, times = length(taus))
+#for(k in 1:length(taus)){
+k = 1
+#Calculate transfer function vector G_tau
+G_tau <- transfer.func(f,taus[k])
+G_tau[1] <- 1
+#G_tau^T * C * G_tau
+out[k] <- t(G_tau)%*%Cov.mat%*%G_tau 
+#}
+return(out)
+#}
+
+
+
 
 
 #Calculate sigma.hat_tau using bandpass variance
--- Code/Sandbox.R
+++ Code/Sandbox.R
@@ -5,9 +5,9 @@
 
 #test out the functions in AVARSpectralEstimate file
 
-X.t <- X.t_missing <- rnorm(2048)
-X.t_missing[c(100:200, 700:950, 1500:1600)] <- NA
-
+X.t <- X.t_missing <- rnorm(100)
+#X.t_missing[c(100:200, 700:950, 1500:1600)] <- NA
+X.t_missing[c(8:13,18,35,51:52)] <- NA
 plot(X.t)
 
 #get spectral estimate
@@ -20,31 +20,24 @@ spec.est <- multitaper_est(X.t_missing,W=0.005, K = 5)
 
 length(spec.est$spectrum)
 
-t.vec <- 1:2048
-test_var <- var_AVAR_trfunc(tapers = spec.est$tapers, t.n =  t.vec[which(!is.na(X.t_missing))], X.t = na.omit(X.t_missing),taus = 2^(0:8))
+t.vec <- 1:100
 
 tapers = spec.est$tapers
 t.n =  t.vec[which(!is.na(X.t_missing))]
 X.t = na.omit(X.t_missing)
-taus = 2^(0:8)
+taus = 2^(0:6)
 
 
 
 #var_AVAR_trfunc <- function(tapers, t.n, X.t, taus){
 
-
+saved <- acf(X.t_missing, na.action = na.pass, lag.max = 100)
   #distance matrix of time points
   dist.mat <- rdist(t.n)
   #R.x matrix
   X.t <- X.t - mean(X.t)
   L <- length(X.t)
-  R.x <- matrix(NA, nrow = L, ncol = L)
-  for(i in 1:L){
-    for(j in 1:L){
-      R.x[i,j] <- saved$acf[abs(t.n[i] - t.n[j]) + 1]
-    }
-  }
-  
+  R.x <- matrix(saved$acf[dist.mat+1], nrow = L, ncol = L)
   #f vector
   f <- seq(0,0.5, length.out = floor(length(t.n))/2 + 1)
   
@@ -52,28 +45,40 @@ taus = 2^(0:8)
   #Calculate Cov matrix C
   N <- length(f)
   Cov.mat <- matrix(NA, nrow = N, ncol = N)
+  W_matlist <- list()
+  
+  for(i in 1:N){
+  print(i)
+    W_matlist[[i]] <- t(tapers)%*%exp(-im*2*pi*f[i]*dist.mat)
+  }
+  
 for(i in 1:N){
   print(i)
-   for(j in 1:N){
-      W.star <- Conj(t(tapers)%*%exp(-im*2*pi*f[i]*dist.mat))
-      W <- t(t(tapers)%*%exp(-im*2*pi*f[j]*dist.mat))
-      Cov.mat[i,j] <- sum(abs(W.star%*%R.x%*%W)^2)
+  j = i
+   while(j>=i & j <=N){
+      W.star <- Conj(W_matlist[[i]])
+      W <- t(W_matlist[[j]])
+      #Cov.mat[i,j] <- sum(abs(W.star%*%R.x%*%W)^2) #frobenius norm 
+      Cov.mat[i,j] <- norm(W.star%*%R.x%*%W, type = "2") #2-norm
+      j = j + 1
     }
-  }
+}
+  
+  Cov.mat[lower.tri(Cov.mat)] <- t(Cov.mat)[lower.tri(Cov.mat)]
   
   out <- rep(NA, times = length(taus))
   #for(k in 1:length(taus)){
   k = 1
     #Calculate transfer function vector G_tau
     G_tau <- transfer.func(f,taus[k])
-    
+    G_tau[1] <- 1
     #G_tau^T * C * G_tau
     out[k] <- t(G_tau)%*%Cov.mat%*%G_tau 
   #}
   return(out)
 #}
       
-      
+
 #R.x matrix
       
 saved <- acf(X.t_missing, na.action = na.pass, lag.max = 2048)
