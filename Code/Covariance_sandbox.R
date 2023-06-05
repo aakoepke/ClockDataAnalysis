87b651ef8be883827f7924a12b3000df87a9b4f7
caitberry <cberry1882@gmail.com>
2023-06-05 11:54:39 -0600

    Looking at variance calculation for spectral based AVAR estimate

--- Code/Covariance_sandbox.R
+++ Code/Covariance_sandbox.R
@@ -1,85 +1,56 @@
 ########### What's the deal? ###########
 ## C_ij = ||W*(f_j)R_xW(f_i)||^2
-library(fields)
-library(RSpectra)
 
-### Look at Bronez method
+source("Code/SA_ImportantFunctions.R")
 
-t.vec <- 1:60 #vector of time points
-t.vec[c(3:8,14, 18, 25, 33:37)] <- NA
-t.vec <- na.omit(t.vec)
-
-dist.mat <- rdist(t.vec)
-W <- 0.19
-A <- sin(2*pi*W*dist.mat)/(pi*dist.mat)
-A[col(A) == row(A)] <- 2*W  #A matrix
-
-eig.vecs <- eigs(A, k = 6, which = "LM")
-eig.vecs <- eigen(A, symmetric = TRUE)
-
-eig.vecs$values
-
-vectors <- eig.vecs$vectors
-
-dim(vectors)
-
-dim(vectors%*%Conj(t(vectors)))
-dim(Conj(t(vectors))%*%vectors)
-
-Conj(t(vectors))%*%vectors
-
-vectors%*%Conj(t(vectors))
-
-eigen(matrix(c(1,1,2,1), ncol = 2))
-
-eigen(matrix(2*c(1,1,2,1), ncol = 2))
-
+im <- complex(real = 0, imaginary = 1)
 
 #### Covariance calculation #####
 
-t.vec <- 1:300
-t.vec[c(20:35,50:63, 100:130)] <- NA
-t.vec <- na.omit(t.vec)
-dist.mat <- rdist(t.vec)
-W <- 0.04
+t.vec <- 1:300 #time vector
+X.t <- rnorm(300) #data
+t.vec[c(20:35,50:63, 100:130)] <- NA #take out values
+X.t[c(20:35,50:63, 100:130)] <- NA #take out values
+t.vec <- na.omit(t.vec) #vector of times with data
+dist.mat <- rdist(t.vec) #distance matrix (delta_nm)
 
-##Generate A
-A <- sin(2*pi*W*dist.mat)/(pi*dist.mat)
-A[col(A) == row(A)] <- 2*W  #A matrix
-  
-##Solve eigenvalue problem for A
-eig.vecs <- eigs(A, k = 6, which = "LM")
+##get multitaper spectral estimate:
+MTSE_test <- multitaper_est(X.t, W = 4/239, K = 5) #calculate tapers
+
 
 ##Get V, V* matrix
-V <- eig.vecs$vectors
-
-V_star <- Conj(t(V))
+V <- MTSE_test$tapers
+dim(V) #columns of V are the eigenvectors, dimension 239 X 5
+V_star <- Conj(t(V)) #dim: 5 x 239, should be real valued so the conjugate isn't really necessary here
+dim(V_star)
 
 ##R_x matrix (identity for white noise)
 
 
 ##Covariance
 N <- length(na.omit(t.vec))
-freq <- seq(0,0.5, length.out = N)
-Cov.mat <- matrix(NA, ncol = N, nrow = N)
+N.fourier <- floor(N/2) + 1
+freq <- seq(0,0.5, length.out = N.fourier)
+Cov.mat <- matrix(NA, ncol = N.fourier, nrow = N.fourier)
 
-for(i in 1:length(freq)){
+for(i in 1:N.fourier){
   print(i)
   j = 1
   while(j <= i){
-    #Cov.mat[i,j] <- exp(freq[i] + freq[j])*norm(V_star%*%V, type = "2") - exp(2*freq[i] + 2*freq[j])*sum(diag(V_star%*%V))
-    Cov.mat[i,j] <- norm(V_star%*%V, type = "2") - sum(diag(V_star%*%V))
+    Cov.mat[i,j] <- norm(V_star%*%exp(im*2*pi*freq[i]*dist.mat)%*%exp(-im*2*pi*freq[j]*dist.mat)%*%V, type = "2")
     j = j+1
   }
 }
 
 Cov.mat[upper.tri(Cov.mat)] <- t(Cov.mat)[upper.tri(Cov.mat)]
-norm(Cov.mat, type= "2")
 
-G_tau <- transfer.func(freq,1)
+#Calculate G_tau vector
+G_tau <- transfer.func(freq,tau = 50) #change the tau value to get different vectors
 G_tau[1] <- 1
-
+#calculate variance for the AVAR estimate at the given tau
 t(G_tau)%*%Cov.mat%*%G_tau*(freq[2])^2
+
+
 
 
 
