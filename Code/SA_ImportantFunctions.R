80692d4a6839e0a9f4a8220d28485099b8c41927
caitberry <cberry1882@gmail.com>
2023-06-12 09:18:27 -0600

    added get_tapers() and MT_spectralEstimate() functions which split up the multitaper_est() function into two parts

--- Code/SA_ImportantFunctions.R
+++ Code/SA_ImportantFunctions.R
@@ -94,6 +94,105 @@ multitaper_est <- function(X.t, W, K){
 }
 
 
+######## the multitaper_est function broken into two parts #####
+####### 1. get_tapers() ########
+####### 2. calculate estimate ######
+
+#input: X.t = time series of length N (possibly with NA values), W = analysis half bandwidth, K = number of tapers
+#output: L x K matrix of tapers where L = length of time series without missing values, K = number of tapers
+get_tapers <- function(X.t, W, K){
+  N.long <- length(X.t)
+  t.n <- 1:N.long
+  missing.indices <- which(is.na(X.t))
+  t.n[which(is.na(X.t))] <- NA
+  
+  
+  dist.mat <- rdist(na.omit(t.n))
+  
+  #create the A' matrix (Chave 2019 equation (22))
+  A.prime <- (1/(pi*dist.mat))*sin(2*pi*W*dist.mat)
+  A.prime[row(A.prime) == col(A.prime)] <- W*2
+  print("A matrix computed")
+  
+  eigdec <- eigs(A.prime, k = K, which = "LM")
+  eig_vecs <- eigdec$vectors #get only the vectors
+  print("tapers computed")
+  
+  if(K ==1){
+    if (mean(Re(eig_vecs))<0){
+      eig_vecs <- -eig_vecs
+    }
+  }
+  
+  if(K == 2  || K == 3){
+    
+    if (mean(Re(eig_vecs[,1]))<0){
+      eig_vecs[,1] <- -eig_vecs[,1]
+    }
+    if (Re(eig_vecs[2,2] - eig_vecs[1,2])<0){
+      eig_vecs[,2] <- -eig_vecs[,2]
+    }
+    
+    if(K == 3){
+      if (mean(Re(eig_vecs[,3]))<0){
+        eig_vecs[,3] <- -eig_vecs[,3]
+      }
+    }
+  }
+  if(K >=4){
+    #some sign maintenance
+    for(i in seq(1,K,by = 2)){
+      if (mean(Re(eig_vecs[,i]))<0){
+        eig_vecs[,i] <- -eig_vecs[,i]
+      }
+    }
+    
+    for(i in seq(2,K-1,by = 2)){
+      if (Re(eig_vecs[2,i] - eig_vecs[1,i])<0){
+        eig_vecs[,i] <- -eig_vecs[,i]
+      }
+    }
+  }
+  
+  print("sign maintenance done")
+  
+  #"tapers" = eig_vecs, "e.values" = eigdec$values,
+  
+  return(eig_vecs)
+} 
+
+#input: X.t = time series of length N with any missing values and length L without, 
+#       V.mat = L X K dimension taper matrix
+#output: freqs = fourier frequencies
+#       spectrum = spectral estimate
+
+MT_spectralEstimate <- function(X.t, V.mat){
+  im <- complex(real = 0, imaginary = 1)
+  X.t <- X.t - mean(X.t, na.rm = TRUE) #demean
+  N.long <- length(X.t)
+  t.n <- 1:N.long
+  missing.indices <- which(is.na(X.t))
+  t.n[which(is.na(X.t))] <- NA
+  t.n_m <- rdist(na.omit(t.n))[1,]
+  
+  ##use tapers to generate spectral estimate
+  N <- length(na.omit(t.n))
+  print(N)
+  S.x.hat <- rep(NA, times = floor(N/2) + 1)
+  freqs <- seq(0,0.5, length.out = floor(N/2) + 1)
+  K <- dim(V.mat)[2]
+  
+  for(j in 1:length(freqs)){
+    k.vec <- rep(NA,times = K)
+    for(k in 1:K){
+      inner.sum <- sum(V.mat[,k]*na.exclude(X.t)*exp(-im*2*pi*freqs[j]*t.n_m))
+      k.vec[k] <- abs(inner.sum)^2
+    }
+    S.x.hat[j] <- mean(k.vec)
+  }
+  return(list("spectrum" = S.x.hat, "freqs" = freqs))
+}
+
 ######################################################################################################
 ###### Calculate sigma.hat_tau using transfer function method (see "reappraisal...", page 70) ########
 ######################################################################################################
