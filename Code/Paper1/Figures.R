4f713c492cda40d809a343e06ff6b255421f70bc
caitberry <cberry1882@gmail.com>
2023-08-18 16:12:30 -0600

    where the figure code will go for the paper

---
+++ Code/Paper1/Figures.R
@@ -0,0 +1,19 @@
+##################################################################
+################### Figures for SA paper 1 #######################
+##################################################################
+
+
+####List of Figures to go in paper (to be re-numbered once paper is finalized) ####
+## 1. Fractional Frequency Deviates
+## 2. Tapers
+## 3. White  Noise no Gaps Simulations
+## 4. White Noise gaps Simulations
+## 5. Flicker Noise no gaps simulations (maybe should choose a different noise type?)
+## 6. Flicker Noise gaps simulations
+
+
+
+##### 4. White Noise no gaps simulations ##########
+
+
+
